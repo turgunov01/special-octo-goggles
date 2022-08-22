9167211f8dca8cf0612700f9567579732ed4ecb8
Sardor <sardor@Rustams-MacBook-Pro.local>
2022-08-22 11:28:18 +0500

    Responsive

--- js/script.js
+++ js/script.js
@@ -46,3 +46,121 @@ var swiper4 = new Swiper(".mySwiper4", {
     initialSlide: 1,
     centeredSlides: true,
 });
+
+let nav = document.querySelector('.nav')
+nav.style.transition = '300ms'
+window.addEventListener('scroll', function () {
+    if (scrollY > 10) {
+        nav.style.background = 'white'
+        nav.style.padding = '2rem 0'
+        nav.style.boxShadow = '0 2px 30px #000'
+    } else {
+        nav.style.background = 'unset'
+        nav.style.padding = 'unset'
+        nav.style.boxShadow = 'unset'
+    }
+})
+
+
+let city = ['Алчевск',
+    'Белая Церковь',
+    'Бердянск',
+    'Борисполь',
+    'Боярка',
+    'Бровары',
+    'Васильков',
+    'Винница',
+    'Горловка',
+    'Днепр',
+    'Донецк',
+    'Житомир',
+    'Запорожье',
+    'Ивано-Франковск',
+    'Ирпень',
+    'Каменское',
+    'Киев',
+]
+let city2 = [
+    'Краматорск',
+    'Кременчуг',
+    'Кривой Рог',
+    'Кропивницкий',
+    'Луганск',
+    'Луцк',
+    'Львов',
+    'Макеевка',
+    'Мариуполь',
+    'Мелитополь',
+    'Николаев',
+    'Никополь',
+    'Обухов',
+    'Одесса',
+    'Павлоград',
+    'Полтава',
+    'Ровно',
+]
+let city3 = [
+    'Славянск',
+    'Суммы',
+    'Тернополь',
+    'Ужгород',
+    'Харьков',
+    'Херсон',
+    'Хмельницкий',
+    'Черкассы',
+    'Чернигов',
+    'Черновцы',
+]
+
+let list = document.getElementById("myList");
+let list2 = document.getElementById("myList2");
+let list3 = document.getElementById("myList3");
+
+city.forEach((item) => {
+    let li = document.createElement("li");
+    li.classList.add('loc__link')
+    li.innerText = item;
+    list.appendChild(li);
+})
+city2.forEach((item2) => {
+    let li = document.createElement("li");
+    li.classList.add('loc__link')
+    li.innerText = item2;
+    list2.appendChild(li);
+})
+city3.forEach((item3) => {
+    let li = document.createElement("li");
+    li.classList.add('loc__link')
+    li.innerText = item3;
+    list3.appendChild(li);
+})
+
+const buttonCollapse = document.querySelectorAll('.accordion-button')
+const itemNotCollapsed = document.querySelectorAll('.accordion-button.collapsed')
+const itemActive = document.querySelectorAll('.accordion__check')
+
+for (let i = 0; i < itemNotCollapsed.length; i++) {
+    for (let a = 0; a < itemActive.length; a++) {
+        itemActive.innerHTML = '+'
+    }
+}
+
+for (let p = 0; p < buttonCollapse.length; p++) {
+
+    if (buttonCollapse[p].classList.contains('.collapsed')) {
+        itemActive.innerHTML = '-'
+    }
+    itemActive.classList.not.contains()
+}
+
+
+// // console.log(itemNotCollapsed)
+// // console.log(itemCollapsed)
+
+// for (let i = 0; i < itemNotCollapsed.length; i++) {
+//     for (let collapse = 0; collapse < array.length; collapse++) {
+//         if (itemNotCollapsed[i].classList.contains(itemCollapsed)) {
+//             console.log('True');
+//         }
+//     }
+// }
